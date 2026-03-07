a2cfdbceefb86932d2faf4e53e4f40a68a19ed73
MelikeBeyazli <162350638+MelikeBeyazli@users.noreply.github.com>
2026-03-07 21:55:30 +0300

    Add joystick manager launch file

---
+++ matrobot_bringup/launch/joystick_manager.launch.py
@@ -0,0 +1,57 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='joy',
+            executable='joy_node',
+            name='joy_node',
+            parameters=[{
+                'deadzone': 0.05,
+                'autorepeat_rate': 20.0,
+            }],
+            output='screen'
+        ),
+
+        Node(
+            package='matrobot_bringup',
+            executable='joy_multi_toggle',
+            name='joy_multi_toggle',
+            parameters=[{
+                'button_a_index': 0,   # A
+                'button_b_index': 1,   # B
+                'button_x_index': 2,   # X
+                'debounce_sec': 0.35,
+
+                # A tuşu -> bringup aç/kapat
+                'command_a': [
+                    'ros2',
+                    'launch',
+                    'matrobot_bringup',
+                    'bringup.launch.py',
+                    'ekf_yaml:=ekf_imu_odom',
+                    'ekf:=false'
+                ],
+
+                # B tuşu -> teleop aç/kapat
+                'command_b': [
+                    'ros2',
+                    'launch',
+                    'matrobot_bringup',
+                    'teleop_cmd_vel.launch.py'
+                ],
+
+                # X tuşu -> slam aç/kapat
+                'command_x': [
+                    'ros2',
+                    'launch',
+                    'matrobot_slam',
+                    'slam_async.launch.py',
+                    'use_rviz:=true'
+                ],
+            }],
+            output='screen'
+        )
+    ])
